555998e97ade056e58e46c3bab2ea5305dd728df
madcowGit <yoengsin@hotmail.com>
2025-08-27 11:04:56 +0200

    render port

--- app.py
+++ app.py
@@ -55,5 +55,9 @@ def scrape():
         "movies": movies
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':    
+    import os
+    
+    port = int(os.environ.get("PORT", 10000))  # Render sets PORT to 10000 by default
+    app.run(host="0.0.0.0", port=port)
+
