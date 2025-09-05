b86034f6c2269e2021eacec37c1f858747af468e
Yoeng Sin Khoe <yoeng_sin.khoe@tno.nl>
2025-09-05 10:53:28 +0200

    isolate test

--- test_imdbscraper.py
+++ test_imdbscraper.py
@@ -21,31 +21,31 @@ class TestIMDBScraper(unittest.TestCase):
         print(json.dumps(response))
         self.assertIn(return_value, json.dumps(response))
 
-class TestAppRoutes(unittest.TestCase):
+# class TestAppRoutes(unittest.TestCase):
 
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+    # def setUp(self):
+        # self.app = app.test_client()
+        # self.app.testing = True
 
-    def test_scrape_movies_route(self):   
-        return_value = app.response_class(
-            response='[{"imdb_id":"tt11655566","type":"Movie"},{"imdb_id":"tt33130902","type":"TV Special"},{"imdb_id":"tt31908384","type":"TV Movie"},{"imdb_id":"tt1118511","type":"Short"}]',
-            status=200,
-            mimetype='application/json'
-        )
-        response = self.app.get('/scrape_movies?list_id=ls569954785')
-        self.assertEqual(response.status_code, 200)
+    # def test_scrape_movies_route(self):   
+        # return_value = app.response_class(
+            # response='[{"imdb_id":"tt11655566","type":"Movie"},{"imdb_id":"tt33130902","type":"TV Special"},{"imdb_id":"tt31908384","type":"TV Movie"},{"imdb_id":"tt1118511","type":"Short"}]',
+            # status=200,
+            # mimetype='application/json'
+        # )
+        # response = self.app.get('/scrape_movies?list_id=ls569954785')
+        # self.assertEqual(response.status_code, 200)
 
-    def test_scrape_tvshows_route(self):   
-        return_value = app.response_class(
-            response='[{"tvdbId":350984},{"tvdbId":251645}]',
-            status=200,
-            mimetype='application/json'
-        )
-        response = self.app.get('/scrape_tvshows?list_id=ls569954785')
-        print(json.dumps(response.response))
-        print(response.status_code)
-        self.assertEqual(response.status_code, 200)
+    # def test_scrape_tvshows_route(self):   
+        # return_value = app.response_class(
+            # response='[{"tvdbId":350984},{"tvdbId":251645}]',
+            # status=200,
+            # mimetype='application/json'
+        # )
+        # response = self.app.get('/scrape_tvshows?list_id=ls569954785')
+        # print(json.dumps(response.response))
+        # print(response.status_code)
+        # self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
