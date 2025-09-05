bd36ce5d9dc405bd201268073642521c7355c8c3
Yoeng Sin Khoe <yoeng_sin.khoe@tno.nl>
2025-09-05 11:36:31 +0200

    check output

--- test_imdbscraper.py
+++ test_imdbscraper.py
@@ -6,26 +6,7 @@ import imdbscraper
 import os
 import json
 
-# Load TVDB API key from GithubActions secrets file
-tvdbapikey = os.environ.get("TVDBAPIKEY")
-    
-    
-print("test imdbscraper.py")
-class TestIMDBScraper(unittest.TestCase):
-    def test_get_movies(self):
-        return_value = json.dumps([{"imdb_id":"tt11655566","type":"Movie"},{"imdb_id":"tt33130902","type":"TV Special"},{"imdb_id":"tt31908384","type":"TV Movie"},{"imdb_id":"tt1118511","type":"Short"}])
-        response = imdbscraper.get_movies('ls569954785')
-        print(json.dumps(response))
-        self.assertIn(return_value, json.dumps(response))
-
-    def test_get_tvshows(self):
-        return_value = json.dumps([{"tvdbId":350984},{"tvdbId":251645}])
-        response = imdbscraper.get_tvshows('ls569954785', tvdbapikey)
-        print(json.dumps(response))
-        self.assertIn(return_value, json.dumps(response))
-
-
-print("test flask app")
+print("test full flask app")
 class TestAppRoutes(unittest.TestCase):
 
     def setUp(self):
@@ -50,7 +31,18 @@ class TestAppRoutes(unittest.TestCase):
         response = self.app.get('/scrape_tvshows?list_id=ls569954785')
         print(response.status_code)
         print(response.get_json())
+        self.assertEqual(json.loads(return_value.response.get_json()), json.loads(response.get_json()))
         self.assertEqual(response.status_code, 200)
+    
+    def test_scrape_movies_watchlist_route(self):   
+        # only check if response is ok
+        return_value = app.response_class(
+            response='[{"imdb_id":0000},{"imdb_id":0000}]',
+            status=200,
+            mimetype='application/json'
+        )
+        response = self.app.get('/scrape_tvshows?list_id=ur18834365')
+        self.assertEqual(response.status_code, 200)    
 
 if __name__ == '__main__':
     unittest.main()
