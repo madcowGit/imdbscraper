e948b7880bad1d674e2d1732ff373057295bd66a
Yoeng Sin Khoe <yoeng_sin.khoe@tno.nl>
2025-09-05 11:51:14 +0200

    clean up workflow

--- test_imdbscraper.py
+++ test_imdbscraper.py
@@ -14,27 +14,31 @@ class TestAppRoutes(unittest.TestCase):
         self.app.testing = True
 
     def test_scrape_movies_route(self):   
+        print("test: scrape movies from user list")
         return_value = app.response_class(
             response='[{"imdb_id":"tt11655566","type":"Movie"},{"imdb_id":"tt33130902","type":"TV Special"},{"imdb_id":"tt31908384","type":"TV Movie"},{"imdb_id":"tt1118511","type":"Short"}]',
             status=200,
             mimetype='application/json'
         )
         response = self.app.get('/scrape_movies?list_id=ls569954785')
+        self.assertEqual(json.loads(return_value.get_data()), json.loads(response.get_data(as_text=True)))
         self.assertEqual(response.status_code, 200)
 
     def test_scrape_tvshows_route(self):   
+        print("test: scrape tvshows from user list")
         return_value = app.response_class(
             response='[{"tvdbId":350984},{"tvdbId":251645}]',
             status=200,
             mimetype='application/json'
         )
         response = self.app.get('/scrape_tvshows?list_id=ls569954785')
-        print(response.status_code)
-        print(response.get_json())
+        #print(response.status_code)
+        #print(response.get_json())
         self.assertEqual(json.loads(return_value.get_data()), json.loads(response.get_data(as_text=True)))
         self.assertEqual(response.status_code, 200)
     
     def test_scrape_movies_watchlist_route(self):   
+        print("test: scrape movies from watchlist")
         # only check if response is ok
         return_value = app.response_class(
             response='[{"imdb_id":0000},{"imdb_id":0000}]',
